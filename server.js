9384d30a2856c064f329f029cca2f7ee6b4e4321
abdul sarnor <teamcodebehind@hotmail.com>
2017-08-31 05:40:13 -0700

    postinstall

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 // app
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/build')));
 
 
 app.listen( process.env.PORT || 8080, () => {
@@ -12,7 +12,7 @@ app.listen( process.env.PORT || 8080, () => {
 });
 
 app.get('/*', (req, res)=> {
-    res.sendFile(path.join(__dirname+'/public/index.html'));
+    res.sendFile(path.join(__dirname+'/build/index.html'));
 });
 
 console.log('running');
