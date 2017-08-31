f3313f622763488792083fe551b8c13daeb23efa
abdul sarnor <teamcodebehind@hotmail.com>
2017-08-31 05:26:49 -0700

    setup for heroku

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+// app
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/build')));
+
+
+app.listen( process.env.PORT || 8080, () => {
+    console.log('This app listening on port 8080!');
+});
+
+app.get('/*', (req, res)=> {
+    res.sendFile(path.join(__dirname+'/build/index.html'));
+});
+
+console.log('running');
+
+
+// -module.exports = app;
